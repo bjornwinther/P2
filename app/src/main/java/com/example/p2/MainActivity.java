d2fb23ee0622516e340a7e99aa15650b387e2f19
Bjørn Winther <bjornwinther@hotmail.com>
2019-04-24 12:22:33 +0200

    LoginScreen almost done

--- app/src/main/java/com/example/p2/MainActivity.java
+++ app/src/main/java/com/example/p2/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText password;
     private TextView numberAttempts;
     private Button login;
+    private int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +27,28 @@ public class MainActivity extends AppCompatActivity {
         numberAttempts = (TextView) findViewById(R.id.tvAttempts);
         login = (Button) findViewById(R.id.btnLogin);
 
+        numberAttempts.setText("No. of attempts remaining: " + String.valueOf(counter));
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(username.getText().toString(), password.getText().toString()); // get input with getText() and convert to string toString()
+
+            }
+        });
+
     }
 
     private void validate(String username, String password) {
-        if((username == "Admin") && (password == "1234")){
-            Intent intent = new Intent(MainActivity.this, secondActivity.class);
+        if((username == "Vangede") && (password == "Gudhjem")){
+            Intent intent = new Intent(MainActivity.this, Forum.class); // correct so it directs to the correct class (not Forum?)
+            startActivity(intent);
+        }else{
+            counter--;
+            numberAttempts.setText("No. of attempts remaining: " + String.valueOf(counter));
+            if(counter <= 0) {
+                login.setEnabled(false); // Deactivates login-button.
+            }
         }
 
     }
