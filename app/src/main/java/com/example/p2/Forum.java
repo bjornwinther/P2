ae1523f2198bf3a1ee4c380c991d06488575ecf6
Patrick Andersen <44160946+sanidk@users.noreply.github.com>
2019-04-24 13:52:55 +0200

    picture

--- app/src/main/java/com/example/p2/Forum.java
+++ app/src/main/java/com/example/p2/Forum.java
@@ -2,7 +2,9 @@ package com.example.p2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -17,6 +19,15 @@ public class Forum extends AppCompatActivity {
     Button back = (Button)findViewById(R.id.BackButton);
     Button options = (Button)findViewById(R.id.OptionsButton);
     Button ask = (Button)findViewById(R.id.BackButton);
+
+    back.setOnClickListener(new View.OnClickListener(){
+        public void onClick(View view){
+            Intent intent = new Intent(Forum.this, MainActivity.class);
+            startActivity(intent);
+        }
+                            }
+
+    );
 
     }
 }
