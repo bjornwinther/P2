bd981ff9363767bed1c9e4d0bf75bbc7aba6687b
Patrick Andersen <44160946+sanidk@users.noreply.github.com>
2019-04-24 12:43:04 +0200

    picture

--- app/src/main/java/com/example/p2/Forum.java
+++ app/src/main/java/com/example/p2/Forum.java
@@ -3,6 +3,8 @@ package com.example.p2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Forum extends AppCompatActivity {
 
@@ -11,7 +13,10 @@ public class Forum extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forum);
 
-
+    EditText search = (EditText)findViewById(R.id.SearchForQuestion);
+    Button back = (Button)findViewById(R.id.BackButton);
+    Button options = (Button)findViewById(R.id.OptionsButton);
+    Button ask = (Button)findViewById(R.id.BackButton);
 
     }
 }
