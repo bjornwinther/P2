107f6fa72a6d89c232f053ee504410dfd678a78a
Patrick Andersen <44160946+sanidk@users.noreply.github.com>
2019-04-24 12:04:09 +0200

    added l

---
+++ app/src/main/java/com/example/p2/Forum.java
@@ -0,0 +1,17 @@
+package com.example.p2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Forum extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forum);
+
+
+
+    }
+}
