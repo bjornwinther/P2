84a8962afa89987f7f743ad86d80ee1d0608f7a3
Bjørn Winther <bjornwinther@hotmail.com>
2019-04-25 11:32:49 +0200

    Fixed missing parts in Sign-up class and Account Class

--- app/src/main/java/com/example/p2/SignUp.java
+++ app/src/main/java/com/example/p2/SignUp.java
@@ -42,7 +42,7 @@ public class SignUp extends AppCompatActivity {
         createAcc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(createPass.getText().toString() != confirmPass.getText().toString()) {
+                if(!createPass.getText().toString().equals(confirmPass.getText().toString())) {
                     Toast.makeText(getApplicationContext(),"Created Password must match Confirmed Password", Toast.LENGTH_SHORT).show();
                 }else{
                     accounts[index] = new Account(username.getText().toString(), email.getText().toString(),
@@ -50,6 +50,7 @@ public class SignUp extends AppCompatActivity {
                     index++;
                     Toast.makeText(getApplicationContext(),"Account created", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(SignUp.this, MainActivity.class);
+                    startActivity(intent);
                 }
             }
         });
