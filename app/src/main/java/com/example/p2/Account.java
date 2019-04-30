74cfca1f001753cb51fe3a2eff18cca07c65e2f7
Bjørn Winther <bjornwinther@hotmail.com>
2019-04-30 10:50:55 +0200

    Corrected stuff in SignUp, Created Profile-activity and class, changed/improved stuff in SignUpSecond.

--- app/src/main/java/com/example/p2/Account.java
+++ app/src/main/java/com/example/p2/Account.java
@@ -7,8 +7,11 @@ public class Account {
     private String phone;
     private String createPass;
     private String confirmPass;
+    //Be aware of default settings of following attributes. Might cause bugs:
     private String gender;
     private int age;
+    private String firstName;
+    private String lastName;
 
 
     public Account(String username, String email, String phone, String createPass, String confirmPass) {
@@ -17,6 +20,22 @@ public class Account {
         this.phone = phone;
         this.createPass = createPass;
         this.confirmPass = confirmPass;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public void setAge(int age) {
