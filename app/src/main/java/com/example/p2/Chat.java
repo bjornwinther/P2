a5081eddc699fd0d7ec135c7f9f6ab73de622f42
MajaSupersonic26 <43847790+MajaSupersonic26@users.noreply.github.com>
2019-05-13 15:42:32 +0200

    header

--- app/src/main/java/com/example/p2/Chat.java
+++ app/src/main/java/com/example/p2/Chat.java
@@ -2,6 +2,7 @@ package com.example.p2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -36,6 +37,13 @@ public class Chat extends AppCompatActivity {
         });
 
     }
-
-
+    //MENU NEW
+    public void showMenu(View view){
+        Intent intent = new Intent(Chat.this, menu.class );
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+    }
+    public void backbutt(View view){
+        finish();
+    }
 }
